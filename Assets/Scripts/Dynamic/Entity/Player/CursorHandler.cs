0d08958d5b8fd0919e4998b35d222fae202327b8
Mark Lipina <mal4739@g.rit.edu>
2018-10-23 21:54:16 -0400

    Made some small changes to shadowcasting to make it more pixel-perfect. Began implementing hot-swapping between mouse control and gamepad control, since cursor and gamepad input behave differently in game

--- Assets/Scripts/Dynamic/Entity/Player/CursorHandler.cs
+++ Assets/Scripts/Dynamic/Entity/Player/CursorHandler.cs
@@ -7,10 +7,10 @@ public class CursorHandler : MonoBehaviour
 {
     [SerializeField] private PlayerHandler _player;
     [SerializeField] private ReticleHandler _reticle;
-    [SerializeField]
-    private CameraScript _camera;
+    [SerializeField] private CameraScript _camera;
 
     private Vector2 _cursorPos;
+    [SerializeField] private bool _usingMouse;
 
 	// Use this for initialization
 	void Start ()
@@ -21,6 +21,26 @@ public class CursorHandler : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        //if during joystick mouse moves or clicks, change to mouse. if during mouse, joystick moves, change to joystick
+
+        //if currently using joystickmouse moves, change to mouse controls
+        if (!_usingMouse && _cursorPos != (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition))
+        {
+            _usingMouse = true;
+        }
+        
+
+
+        if (_usingMouse)
+        {
+
+        }
+        else
+        {
+
+        }
+
+
         _cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = _cursorPos;
 
@@ -28,4 +48,31 @@ public class CursorHandler : MonoBehaviour
         _player.UpdateMousePosition(_cursorPos);
         _camera.UpdateMousePosition(_cursorPos);
 	}
+    /*
+    private void OnEnable()
+    {
+        ReInput.players.Players[0].AddInputEventDelegate(SwitchToGamepad,
+            UpdateLoopType.Update,
+            InputActionEventType.ButtonJustPressed,
+            ReInput.mapping.GetMouseLayout(0).);
+    }
+
+    private void OnDisable()
+    {
+        ReInput.eve
+    }
+
+
+    private void SwitchToGamepad()
+    {
+
+    }
+
+    private void SwitchToMouse()
+    {
+
+    }
+    */
+
+
 }

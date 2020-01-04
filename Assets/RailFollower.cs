4cebfdc818d3d0bee42cd083836dfb57c182ffcb
Mark Lipina <mlipina1234@gmail.com>
2020-01-04 16:09:15 -0500

    Modified cataclysm cinematic area, added railfollower for camera control during cutscenes

---
+++ Assets/RailFollower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailFollower : MonoBehaviour
+{
+
+    [SerializeField] bool _followsXAxis = false;
+    [SerializeField] bool _followsYAxis = false;
+    [SerializeField] Transform _target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 futurePosition = transform.position;
+
+        if (_followsXAxis)
+        {
+            futurePosition.x = _target.position.x;
+        }
+        if (_followsYAxis)
+        {
+            futurePosition.y = _target.position.y;
+        }
+        transform.position = futurePosition;
+    }
+}

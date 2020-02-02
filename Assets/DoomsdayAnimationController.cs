265606a5527741d7a77cf82df798d42428351429
Mark <mlipina1234@gmail.com>
2020-02-02 11:46:55 -0500

    Added more art to doomsday cinematic, created class for animation of the doomsday cinematic

---
+++ Assets/DoomsdayAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the background animation in the "doomsday" scenario on the CollapsingBalconyLedge scene
+/// </summary>
+public class DoomsdayAnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //=================================| STAGES |===================================
+     
+    /// <summary>
+    /// Looping, default animation 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator OpeningLoop()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+}
--- Assets/Scripts/Cinematics/BackgroundParallax.cs
+++ Assets/Scripts/Cinematics/BackgroundParallax.cs
@@ -27,8 +27,15 @@ public class BackgroundParallax : MonoBehaviour
         //Perform parallax motion
         //when camera is dead-on on top of the object's original position the object should be at its original position
         //when the camera moves, the object is slightly pulled in the direction of the camera to reflect its Depth field
+        /*
         Vector3 parallax = _camera.transform.position - _originalPosition;
         parallax = new Vector3(parallax.x * _strength.x, parallax.y * _strength.y, 0f);
         transform.position = _originalPosition + parallax;
+        */
+        transform.position = new Vector3(Mathf.Lerp(_originalPosition.x, _camera.transform.position.x, _strength.x), Mathf.Lerp(_originalPosition.y, _camera.transform.position.y, _strength.y), transform.position.z);
+        if (_strength.x == 1.0f && _strength.y == 1.0f)
+        {
+            transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, transform.position.z);
+        }
     }
 }
